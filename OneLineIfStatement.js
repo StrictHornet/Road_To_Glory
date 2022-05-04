260801eed82b8f8240da1baf9385a9ce603ec251
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2022-05-04 22:46:15 +0100

    Create one liner

---
+++ OneLineIfStatement.js
@@ -0,0 +1,6 @@
+//One line if statement
+if (lemons) return "foo gave me a bar";
+
+//Compare consecutive elements of array in one traversal
+//e.g [1,2,3,4] i.e 1 & 2; 3 & 4
+if (nums[2*i-1] == nums[2*i]){condition}
